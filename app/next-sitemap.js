b19b59b21a4aa2a090074f9623252aedbe7e0007
basat <itzbasatmaqsood@gmail.com>
2025-04-27 10:18:45 +0500

    fixing

--- app/next-sitemap.js
+++ app/next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: 'https://basatmaqsood.com', // Your primary domain
+    siteUrl: 'https://portfolio-new-alpha-two.vercel.app/', // Your primary domain
     generateRobotsTxt: true, // Optionally generate a robots.txt
     changefreq: 'daily', // Frequency of content changes
     priority: 0.7, // Default priority of pages
